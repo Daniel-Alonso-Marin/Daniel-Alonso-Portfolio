e7f9d167f7f784a2cb79882aa9a55de0dd9fcd33
Daniel <alonsomarindaniel@gmail.com>
2021-08-01 12:19:29 +0200

    1.3

--- Scripts/earthPlanet.js
+++ Scripts/earthPlanet.js
@@ -9,13 +9,46 @@ let img;
 let sSize;
 let canvas;
 
+
+let screenSize = windowSize();
+let screenHeight = screenSize[1];
+let screenWidth = screenSize[0];
+
+function windowSize() {
+  var tam = [0, 0];
+  if (typeof window.innerWidth != 'undefined')
+  {
+    tam = [window.innerWidth,window.innerHeight];
+  }
+  else if (typeof document.documentElement != 'undefined'
+      && typeof document.documentElement.clientWidth !=
+      'undefined' && document.documentElement.clientWidth != 0)
+  {
+    tam = [
+        document.documentElement.clientWidth,
+        document.documentElement.clientHeight
+    ];
+  }
+  else   {
+    tam = [
+        document.getElementsByTagName('body')[0].clientWidth,
+        document.getElementsByTagName('body')[0].clientHeight
+    ];
+  }
+  return tam;
+}
+
+
+//Start the p5.js code 
+
 function preload(){
   img = loadImage('Images/earth.jpg');
 }
 
 
-function setup() {
-  createCanvas(800, 800,WEBGL);
+function setup(){
+
+  createCanvas(screenWidth-20, screenHeight-75,WEBGL);
 
   noStroke();
   noFill();
@@ -25,20 +58,20 @@ function setup() {
   canvas.background(0);
   canvas.fill(43,190,200,2);
   canvas.noStroke();
-  for(let i = 900;i > 0;i-=12){
-    canvas.circle(400,400,i);
+  for(let i = 800;i > 0;i-=12){
+    canvas.circle(width/2,height/2,i);
   }
   canvas.endShape();
   
 }
 
 function draw() {
-  image(canvas,-400,-400);
+  image(canvas,-width/2,-height/2);
   //background(0);
   //translate(400,400,0);
   
   spotLight(220, 220, 240, 0, 0, 900, 0, 0, -1, radians(15), 0);
-  ambientLight(20);
+  ambientLight(30);
   
   rotateY(y);
   rotateX(x);
